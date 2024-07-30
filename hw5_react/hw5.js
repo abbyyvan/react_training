5f766ebf52ff87287e8c7c6215c24a8aaf16a5f1
abbyyvan <47904921+abbyyvan@users.noreply.github.com>
2024-07-29 21:46:19 -0400

    hw5 react basic

---
+++ hw5_react/hw5.js
@@ -0,0 +1,18 @@
+//1, What’s package.json and package-lock.json?
+//package.json is using NPM to manage project dependencies and project information
+//package-lock.json contains exact versions of the denpendencies
+
+//2. What’s the difference between npm and npx?
+//npm is node package manager. it is used to install packages from npm regisry
+//npx is a package runner tool come with npm. it allows you to execure ninaries from packages that aren't globallyinstalled
+//ie. npx create-react-app my-app runs the create-react-app command without needing to have it installed globally.
+
+//3.What is babel?
+// Babel is a javascript compiler that allows you to use ES6 and above version of javascipt feature by transforming them into a version which is compatible with older browsers or environments\
+// it can also transform JSX into plain JS
+//it provides plugins and presets to customize how code is tranformed
+
+//4. What is webpack?
+//webpack is a module bundler for js application. it can bundle js files, and HTML, CSS and images by using loaders
+// it hekos optimize and manage the code and asset for production, including features like code splitting, minification and hot module replacement.
+
