0d1234681f3c0ce204de6037db5078ae59f87163
abbyyvan <47904921+abbyyvan@users.noreply.github.com>
2024-07-31 14:47:38 -0400

    update concept questiosn

--- hw6_react_2/hw6_QA.js
+++ hw6_react_2/hw6_QA.js
@@ -3,10 +3,16 @@
 //it will help your manage state changes 
 
 //2.What is props drilling and state lifting?
-//props
+//props drilling refers to the process of passing data from a parent component to a nested child compnent.
+//it goes through components in between
+//state lift is the move the state up tp the closet common ancestpr pf 
 
 //3.What is the ‘key’ attribute?
+//key attribute is used in Reacrt to uniquely identify elelments in an array of elements
 
 //4.What is synthetic event?
+//a syntheti event is cross-browser wrapper around the browser's native event. it combines the behavior of different browsers into one API.
+
 
 //5.What is virtual dom?
+//virtual dom is an in-memory representation of the real DOM elememnts
