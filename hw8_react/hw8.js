7bfb23ceb2ee76af1b4fe118c51e16f394d7d663
abbyyvan <47904921+abbyyvan@users.noreply.github.com>
2024-08-01 23:14:59 -0400

    hw8

---
+++ hw8_react/hw8.js
@@ -0,0 +1,13 @@
+//1. What are some differences between class and functional components?
+//syntax: CC requires 'render' method, FC simpler
+//state manager: class use this.state and this.setState; FC use useState hook
+//lifecycle: CC 1.componentDidMount, componentDidUpdate, componentWillUpdate; FC, useEffect hook to handle side effect to mimic lifecycle method
+//performance: CC less efficient; FC general faster and more lightweight
+//this binding: CC need to bind this to method, FC
+
+//2.Explain what lifecycle is in a simple way. How do you manage it in class and functional components?
+//lifecycle decides when a component is created, updated and removed from DOM.
+
+
+//3. Explain immutability in one sentence.
+//immutabilty means that once the data is created , it cannot be modified; instead new versions are created for any changes.
