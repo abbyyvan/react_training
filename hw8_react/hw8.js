7878de8eac7aa26660c6167cad182ecc78b86509
abbyyvan <47904921+abbyyvan@users.noreply.github.com>
2024-08-05 14:16:55 -0400

    hw9 concept question

--- hw8_react/hw8.js
+++ hw8_react/hw8.js
@@ -1,9 +1,9 @@
 //1. What are some differences between class and functional components?
-//syntax: CC requires 'render' method, FC simpler
+//syntax: CC is class that extends React.component。 it requires 'render' method; FC is just function that return JSX. so it doesn't need render method. the return value of the function is the JSX to be rendered
 //state manager: class use this.state and this.setState; FC use useState hook
 //lifecycle: CC 1.componentDidMount, componentDidUpdate, componentWillUpdate; FC, useEffect hook to handle side effect to mimic lifecycle method
 //performance: CC less efficient; FC general faster and more lightweight
-//this binding: CC need to bind this to method, FC
+//this binding: CC needs to bind this to method; for FC, we don't need to worry about it. 
 
 //2.Explain what lifecycle is in a simple way. How do you manage it in class and functional components?
 //lifecycle decides when a component is created, updated and removed from DOM.
